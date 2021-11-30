71a172372addd1b30bc2796e359e3faa597912cc
Niklas Vejlby Schjoldager <nikl1565@stud.kea.dk>
2021-11-30 13:56:52 +0100

    Astro + Svelte now works properly (thankgod)

--- src/components/Menu.svelte
+++ src/components/Menu.svelte
@@ -1,7 +1,18 @@
----
+<script>
+  let menuActive = false;
+</script>
 
----
+<button on:click={() => (menuActive = !menuActive)}>
+  x       
+</button>
+<nav class:menuActive>
+  <ul>
+    <li>test</li>
+  </ul>
+</nav>
 
-
-
-<style></style>
+<style>
+  nav.menuActive{
+    color: red;
+  }
+</style>
