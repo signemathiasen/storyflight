af31c1160cdab96443e98d5e38d2546074e59ce7
Linea <line301u@stud.kea.dk>
2021-11-30 13:05:32 +0100

    menu not working

--- src/components/Menu.svelte
+++ src/components/Menu.svelte
@@ -1,7 +1,19 @@
----
-
----
-
-
-
-<style></style>
+<script>
+    let menuActive = false;
+</script>
+  
+  <button on:click={() => (menuActive = !menuActive)}>
+    x       
+  </button>
+  <nav>
+    <ul>
+      <li>test</li>
+    </ul>
+  </nav>
+  
+  <style>
+    nav.menuActive{
+    color: red;
+}
+  </style>
+  
