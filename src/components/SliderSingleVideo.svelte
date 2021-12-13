7206523c35f3bfa02ceb7d87e50f6bc4a41cc6ef
Niklas Vejlby Schjoldager <nikl1565@stud.kea.dk>
2021-12-13 13:32:27 +0100

    IOS autoplay

--- src/components/SliderSingleVideo.svelte
+++ src/components/SliderSingleVideo.svelte
@@ -16,7 +16,7 @@
 <div class="case-wrap">
     <a href={`/cases/${singleVideoData.slug}`} class="video-wrap">
         <img bind:this={previewImage} class="preview-image" src={singleVideoData.case_image.guid} alt={singleVideoData.case_image.post_title} loading="lazy">
-        <video class="preview-video" on:mouseenter={playVideo} on:mouseout={pauseVideo} muted={'true'} preload="auto">
+        <video class="preview-video" on:mouseenter={playVideo} on:mouseout={pauseVideo} muted={'true'} preload="auto" playsinline>
             <source src={singleVideoData.preview_video.guid} type="video/mp4">
           </video>
     </a>
